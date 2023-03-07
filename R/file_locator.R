40384cb7328f1ff5d9b6a22a134cea710663e916
abhimishr <A.Mishra@cgiar.org>
2023-03-07 14:51:43 -0500

    dummy package commit

---
+++ R/file_locator.R
@@ -0,0 +1,15 @@
+#' File locator
+#'
+#' @param source Path to IMPACT model run
+#'
+#' @return directory
+#' @export
+#'
+#' @examples
+#' x <- file_locator()
+#' 
+#' @author Abhijeet Mishra
+
+file_locator <- function(source){
+  if(!dir.exists(source)) stop("Invalid source folder. Provide path to model folder.")
+}
