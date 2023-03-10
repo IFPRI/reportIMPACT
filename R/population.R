7d49a62ce673a0f6d638df4028048c025f7cada2
abhimishr <A.Mishra@cgiar.org>
2023-03-10 16:53:24 -0500

    build success

--- R/population.R
+++ R/population.R
@@ -3,22 +3,23 @@
 #' @param gdx final GDX from an IMPACT run
 #'
 #' @return GLO population
-#' @export
 #'
-#' @import dplyr
-#'
-#' @examples
-#' population_glo <- population(gdx)
+#' @import gdxrrw
+#' @importFrom dplyr group_by summarise %>%
+#' @importFrom rlang .data
 #' @author Abhijeet Mishra
+#' @examples
+#' \dontrun{x <- population(gdx)}
+#' @export
 
 population <- function(gdx){
-  var <- gdxrrw::rgdx.param(gdxName = gdx, "PopX0")
-  colnames(var)[length(colnames(var))] <- "value"
-  df <- var %>%
-    group_by(YRS) %>%
-    summarise(value = sum(value))
+  df <- rgdx.param(gdxName = gdx, "PopX0")
+  colnames(df)[length(colnames(df))] <- "value"
+  df <- df %>%
+    group_by(.data$YRS) %>%
+    summarise(value = sum(.data$value))
   df$identifier <- gsub(pattern = ".gdx",replacement = "",x = basename(gdx))
-  df$source <- "IMPACT"
+  df$model <- "IMPACT"
   df$variable <- "Population"
   df$unit <- "million capita"
   out <- as.data.frame(df)
