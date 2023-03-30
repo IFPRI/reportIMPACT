f5a37e484778410e07022422f1c8097c720ff180
abhimishr <A.Mishra@cgiar.org>
2023-03-30 13:49:36 -0400

    improvements

--- R/clean_description.R
+++ R/clean_description.R
@@ -26,5 +26,32 @@ clean_description <- function(df){
     df$indicator <- paste(df$indicator,df$long_name,sep = "|")
   }
 
+  if(is.element("h", colnames(df))){
+    df$indicator <- paste(df$indicator,df$h,sep = "|")
+  }
+
+  if(is.element("fctr", colnames(df))){
+    df$fctr <- gsub(pattern = "air",replacement = "Irrigated",x = df$fctr)
+    df$fctr <- gsub(pattern = "arf",replacement = "Rainfed",x = df$fctr)
+
+    df$fctr <- gsub(pattern = "gir",replacement = "Irrigated grass",x = df$fctr)
+    df$fctr <- gsub(pattern = "grf",replacement = "Rainfed grass",x = df$fctr)
+
+    df$fctr <- gsub(pattern = "flbr",replacement = "Labor",x = df$fctr)
+    df$fctr <- gsub(pattern = "fert",replacement = "Fertilizer",x = df$fctr)
+
+    df$indicator <- paste(df$indicator,df$fctr,sep = "|")
+  }
+
+  if(is.element("lnd", colnames(df))){
+    df$fctr <- gsub(pattern = "air",replacement = "Irrigated",x = df$lnd)
+    df$fctr <- gsub(pattern = "arf",replacement = "Rainfed",x = df$lnd)
+
+    df$fctr <- gsub(pattern = "gir",replacement = "Irrigated grass",x = df$lnd)
+    df$fctr <- gsub(pattern = "grf",replacement = "Rainfed grass",x = df$lnd)
+
+    df$indicator <- paste(df$indicator,df$lnd,sep = "|")
+  }
+
   return(df)
 }
--- R/getReport.R
+++ R/getReport.R
@@ -3,6 +3,7 @@
 #' @param gdx final GDX from an IMPACT run
 #' @param prep_flag Who processed data
 #' @param export if RDS file should be written
+#' @param base_year Base year on which relative index can be calculated
 #'
 #' @return Full data output
 #'
@@ -15,18 +16,103 @@
 
 getReport <- function(gdx,
                       prep_flag = "Default",
-                      export=TRUE){
+                      export=TRUE,
+                      base_year = 2005){
   message("Start getReport(gdx)...")
   t <- Sys.time()
 
   out_list <- list()
   cols <- suppressMessages(colnames(reportPopulation(gdx)))
-  out_list[["Population"]] <- reportPopulation(gdx)
-  out_list[["Animal Numbers"]] <- reportAnimals(gdx)
-  out_list[["Export quantity"]] <- reportExport(gdx)
 
+  # Custom function for additional calcs ----
+  additional_calculation <- function(base_list,
+                                     name,
+                                     base_year) {
+    stopifnot(is.list(out_list))
+    stopifnot(is.character(name))
+    stopifnot(is.numeric(base_year))
+
+    dummy <- list()
+
+    dummy[[paste0(name," (relative)")]] <- calcRelative(df = base_list[[name]],
+                                                           base_year = base_year)
+    dummy[[paste0(name," (index)")]]    <- calcRelative(df = base_list[[name]],
+                                                           base_year = base_year,
+                                                           type = "index")
+    return(append(base_list,dummy))
+  }
+
+  # Population ----
+  name = "Population"
+  out_list[[name]]            <- reportPopulation(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Household Population ----
+  name = "Household Population"
+  out_list[[name]]            <- reportHouseholdPopulation(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Animal Numbers ----
+  name = "Animal Numbers"
+  out_list[[name]]            <- reportAnimals(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Export Quantity ----
+  name = "Export quantity"
+  out_list[[name]]            <- reportExport(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Net Trade ----
+  name = "Net Trade"
+  out_list[[name]]            <- reportNetTrade(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Food Availability ----
+  name = "Food Availability"
+  out_list[[name]]            <- reportFoodAvailability(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Crop Area ----
+  name = "Crop Area"
+  out_list[[name]]            <- reportCropArea(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # GDP ----
+  name = "GDP"
+  out_list[[name]]            <- reportGDP(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Household Income ----
+  name = "Household Income"
+  out_list[[name]]            <- reportHouseholdIncome(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Hunger Risk ----
+  name = "Hunger Risk"
+  out_list[[name]]            <- reportHungerRisk(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Hunger Risk ----
+  name = "Biofuel feedstock"
+  out_list[[name]]            <- reportBiofuelFeedstock(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Demand ----
+  name = "Demand"
+  out_list[[name]]            <- reportDemand(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+  # Household Demand ----
+  name = "Household demand"
+  out_list[[name]]            <- reportHouseholdDemand(gdx)
+  out_list <- additional_calculation(base_list = out_list,name = name,base_year = base_year)
+
+
+  # Combine Results
   out <- rbindlist(out_list,use.names=TRUE, fill=TRUE)
-  out <- out[,cols,with = FALSE]
+  out$unit2[is.na(out$unit2)] = "Default"
+
+  # Add flag to idnetify
   out$prep_flag <- prep_flag
 
   if (export){
@@ -34,5 +120,5 @@ getReport <- function(gdx,
     saveRDS(object = out, file = export_dir)
     message("Results exported to ", export_dir,"\n")
   } else return(out)
-  message("Finished post processing in ",round(difftime(Sys.time(), t, units='mins'),1), " minutes")
+  message("\nFinished post processing in ",round(difftime(Sys.time(), t, units='mins'),1), " minutes")
 }
