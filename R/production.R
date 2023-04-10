1eefc4c314b76e2a68a55035bbd9c2d1dd689bdf
abhimishr <A.Mishra@cgiar.org>
2023-04-10 09:35:48 -0400

    add production

--- R/production.R
+++ R/production.R
@@ -12,5 +12,17 @@
 
 production <- function(gdx){
   df <- aggregateIMPACT(df = readGDX(gdx = gdx,name = "QSX0"))
+
+  df_mag <- as.magpie(df)
+  trade_sets <- grep(pattern = "raded",x = getNames(df_mag),value = TRUE)
+  traded_agg <- df_mag[,,trade_sets]
+  traded_agg <- add_dimension(x = dimSums(x = traded_agg,dim = "groups",na.rm = TRUE),add = "groups",nm = "Combined",dim = 3.3)
+
+  df <- mbind(df_mag,traded_agg)
+
+  df <- as.data.frame(df)[-1]
+
+  colnames(df) <- c(getSets(df_mag),"value")
+
   return(df)
 }
